752521c1f00f9024480d40e8a5585658f4f32b51
Skylar Payne <skylar.b.payne@gmail.com>
2014-12-18 22:37:15 -0800

    Added an idea submission page

--- app/routes.py
+++ app/routes.py
@@ -22,5 +22,9 @@ ideas = [
 def index():
     return render_template('index.html', ideas=ideas)
 
+@app.route('/new', methods=['GET'])
+def new():
+    return render_template('new.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
