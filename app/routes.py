d152a5de4b771bca7e7edd5877de0f82d54efdca
Skylar Payne <skylar.b.payne@gmail.com>
2014-12-18 22:04:31 -0800

    display submitted ideas

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,24 @@ from flask import render_template
 
 app = Flask(__name__)
 
+ideas = [
+    {
+        'author': 'Skylar',
+        'idea': 'robot hands'
+    },
+    {
+        'author': 'Rick',
+        'idea': 'motorized hammer'
+    },
+    {
+        'author': 'Laina',
+        'idea': 'kitten incubator'
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', ideas=ideas)
 
 if __name__ == "__main__":
     app.run(debug=True)
